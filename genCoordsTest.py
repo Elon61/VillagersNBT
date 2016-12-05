8433e49bb5121d76c55e18c64408da02a8997748
Elon61 <Hi>
2016-12-05 08:10:24 +0200

    Wheeee

---
+++ genCoordsTest.py
@@ -0,0 +1,19 @@
+def testnameplsreplace(x1, x2, y, z1, z2):
+    """
+    make a MCEDIT filter to do the same thing could be cool,
+    like a filter to create a village on every selected door.
+    """
+    k = []
+    if x1 > x2:
+        x1, x2 = x2, x1
+    if z1 > z2:
+        z1, z2 = z2, z1
+    for x in xrange(x1, x2 + 1):
+        for z in xrange(z1, z2 + 1):
+            #k.append([x, y, z])
+            k.extend([[x, y, z]])
+    k.append("end")
+    #k.pop()
+    return k
+
+print testnameplsreplace(1, -10, 'Y', -1, 10)
