d81b12c9a99f4b93660e3cfea7a800323169b5d3
Elon <bestbananaever%gg>
2016-12-16 17:15:06 +0200

    I think i ate a little bit of potatoes, just a little though

--- genCoordsTest.py
+++ genCoordsTest.py
@@ -4,13 +4,16 @@ def testnameplsreplace(x1, x2, y, z1, z2):
     like a filter to create a village on every selected door, or on a row of doors on the X/Z axis.
     """
     k = []
+    j = []
     if x1 > x2:
         x1, x2 = x2, x1
     if z1 > z2:
         z1, z2 = z2, z1
     for x in xrange(x1, x2 + 1):
         for z in xrange(z1, z2 + 1):
-            k.append([x, y, z])
+            j.append([x, y, z])
+        k.append(j)
+        j = []
     return k
 
 print testnameplsreplace(1, 3, 'Y', 2, 4)
