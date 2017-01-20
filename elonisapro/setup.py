8cc2ee925db6200d15e8f1c71beb319bb346fb59
A4Vision <asafyi@gmail.com>
2017-01-20 16:14:58 +0200

    BA ``

--- elonisapro/setup.py
+++ elonisapro/setup.py
@@ -16,7 +16,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1',
 
-    description='A sample Python project',
+    description=':(',
     long_description=long_description,
 
     # The project's main homepage.
